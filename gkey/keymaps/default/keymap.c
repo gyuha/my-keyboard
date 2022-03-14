126a7cccd69bf1515aeea28ab08ee83b566f5ad9
gyuha <nicegyuha@gmail.com>
2022-03-14 09:41:52 +0900

    펌웨어 업데이트

--- gkey/keymaps/default/keymap.c
+++ gkey/keymaps/default/keymap.c
@@ -3,7 +3,8 @@
 
 enum layer_names {
    _QWERTY,
-   _FN1
+   _FN1,
+   _FN2
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -12,12 +13,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_PSCR,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
       KC_F5,    MO(_FN1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,           KC_PGUP,
       KC_INS,   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-      KC_DEL,   KC_LCTL,  KC_LWIN, KC_LALT, KC_APP,  KC_SPC,               KC_RALT, KC_SPC,           KC_RALT, MO(_FN1), KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT),
+      KC_DEL,   KC_LCTL,  KC_LWIN, KC_LALT, MO(_FN2),KC_SPC,               KC_RALT, KC_SPC,           KC_RALT, MO(_FN1), KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT),
    [_FN1] = LAYOUT(
-      RESET  ,  _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  KC_F12,  KC_F12,
+      RESET  ,  _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  _______, KC_MUTE,
       _______,  _______,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,     _______, _______, _______, _______,  KC_PGUP,  _______, _______, _______, _______,
-      _______,  _______,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______,  _______, _______,          _______,
-      _______,  _______,  _______, _______, _______, _______, _______,     KC_PGDN, _______, _______, _______,  _______,  _______, _______, _______, _______,
+      _______,  _______,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______,  _______, _______,          KC_VOLU,
+      _______,  _______,  _______, _______, _______, _______, _______,     KC_PGDN, _______, _______, _______,  _______,  _______, _______, _______, KC_VOLD,
+      _______,  _______,  _______, _______, _______, _______,              _______, _______,          _______,  _______,  _______, _______, _______, _______),
+   [_FN2] = LAYOUT(
+      RESET  ,  _______,  KC_KP_1, KC_KP_2, KC_KP_3, KC_F4,   KC_KP_5,     KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9,  KC_KP_0,  KC_PMNS, KC_PEQL,  _______,KC_P1,
+      _______,  _______,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,     KC_KP_7, KC_KP_8, KC_KP_9, _______,  _______,  _______, _______, _______, KC_P2,
+      _______,  _______,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,     KC_KP_4, KC_KP_5, KC_KP_6, _______,  _______,  _______, KC_PENT,          KC_MPRV,
+      _______,  _______,  _______, _______, _______, _______, _______,     KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3,  _______,  _______, _______, _______, KC_MNXT,
       _______,  _______,  _______, _______, _______, _______,              _______, _______,          _______,  _______,  _______, _______, _______, _______)
 };
 
