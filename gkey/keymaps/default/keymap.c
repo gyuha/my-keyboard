6e21992c412a80b0266da81b919bdad35ed92ea3
gyuha <nicegyuha@gmail.com>
2022-04-17 08:53:23 +0900

    update

--- gkey/keymaps/default/keymap.c
+++ gkey/keymaps/default/keymap.c
@@ -16,15 +16,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL,  KC_LWIN, KC_LALT, MO(_FN2),KC_SPC,                     KC_RALT, KC_SPC,           KC_RALT, MO(_FN1), KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT),
    [_FN1] = LAYOUT(
       KC_GRV ,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,  KC_F12,  _______, KC_MUTE,
-      KC_CAPS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,           _______, KC_GRV , _______, _______,  KC_PGUP,  _______, _______, _______, _______,
+      KC_CAPS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,           _______, KC_GRV , _______, _______,  KC_PGUP,  _______, _______, _______, KC_MPLY,
       _______,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______,  _______, _______,          KC_VOLU,
-      _______,  _______, _______, _______, _______, _______,           KC_PGDN, KC_PGDN, _______, _______,  KC_INS ,  KC_DEL , _______, _______, KC_VOLD,
-      _______,  _______, _______, _______, _______,                    _______, _______,          _______,  _______,  _______, _______, _______, _______),
+      _______,  KC_INS,  KC_DEL,  KC_PSCR, _______, _______,           KC_PGDN, KC_PGDN, _______, _______,  KC_INS ,  KC_DEL , _______, _______, KC_VOLD,
+      _______,  _______, _______, _______, _______,                    KC_CAPS, _______,          _______,  _______,  _______, _______, _______, _______),
    [_FN2] = LAYOUT(
-      RESET  ,  KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_F6, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9,  KC_KP_0,  KC_PMNS, KC_PEQL,  _______,KC_P1,
-      _______,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,           KC_KP_7, KC_KP_8, KC_KP_9, _______,  _______,  _______, _______, _______, KC_P2,
+      RESET  ,  KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_F6, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9,  KC_KP_0,  KC_PMNS, KC_PEQL, _______, KC_MUTE,
+      _______,  KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______,           KC_KP_7, KC_KP_8, KC_KP_9, _______,  _______,  _______, _______, _______, KC_SLEP,
       _______,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______,           KC_KP_4, KC_KP_5, KC_KP_6, _______,  _______,  _______, KC_PENT,          KC_MPRV,
-      _______,  _______, _______, _______, _______, _______,           KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3,  _______,  _______, _______, _______, KC_MNXT,
+      _______,  KC_INS,  KC_DEL,  KC_PSCR, _______, _______,           KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3,  KC_INS,   KC_DEL,  _______, _______, KC_MNXT,
       _______,  _______, _______, _______, _______,                    KC_NUM , _______,          _______,  _______,  _______, _______, _______, _______)
 };
 
